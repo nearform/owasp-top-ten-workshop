8e1f3fbf4e4d3e19860bf6f1cd13a77f4f6830f1
Marco Ippolito <marcoippolito54@gmail.com>
2022-09-20 10:00:02 +0200

    feat: created step 5 cookies no security

---
+++ migrations/005.do.create-table.sql
@@ -0,0 +1,16 @@
+INSERT INTO users (
+  username,
+  password,
+  age,
+  credit_card_number
+) VALUES (
+  'alice', 
+  '482c811da5d5b4bc6d497ffa98491e38', 
+  23, 
+  '1234567890123456'
+), (
+  'bob', 
+  '884a22eb30e5cfd71894d43ac553faa5', 
+  31, 
+  '1234567890123456'
+);
