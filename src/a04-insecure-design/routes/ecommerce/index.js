488a5fc765d2db67bf3a1f55b93038945a9b611c
Washington Soares <washingtonudi@gmail.com>
2022-09-20 22:17:00 -0300

    feat: update ecommerce index

--- src/a04-insecure-design/routes/ecommerce/index.js
+++ src/a04-insecure-design/routes/ecommerce/index.js
@@ -2,20 +2,9 @@ import solution from './solution.js'
 import { getSolutionToExport } from 'owasp-shared/export-solution.js'
 
 async function ecommerce(fastify) {
-  fastify.post(
-    '/buy-product',
-    {
-      config: {
-        rateLimit: {
-          max: 2,
-          timeWindow: '1 minute'
-        }
-      }
-    },
-    (req, reply) => {
-      reply.send({ success: true })
-    }
-  )
+  fastify.post('/buy-product', (req, reply) => {
+    reply.send({ success: true })
+  })
 }
 
 // Note: This helper just helps with internal unit testing
