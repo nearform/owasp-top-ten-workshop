3b31d902e070e9b56425dc05ebfd187225960676
Tomasz Mikos <tomasz.mikos@nearform.com>
2022-09-29 14:51:21 +0200

    fix: review fixes (#51)

--- src/a02-cryptographic-failure/utils/solution.js
+++ src/a02-cryptographic-failure/utils/solution.js
@@ -1,28 +1,11 @@
-import bcrypt from 'bcrypt'
+import { hash, compare } from 'bcrypt'
 
 const saltRounds = 10
+
 export async function encryptPassword(password) {
-  return new Promise((resolve, reject) => {
-    bcrypt.genSalt(saltRounds, (err, salt) => {
-      bcrypt.hash(password, salt, (err, hash) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(hash)
-        }
-      })
-    })
-  })
+  return await hash(password, saltRounds)
 }
 
 export async function comparePassword(password, hash) {
-  return new Promise((resolve, reject) => {
-    bcrypt.compare(password, hash, (err, result) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(result)
-      }
-    })
-  })
+  return await compare(password, hash)
 }
